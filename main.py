72700ae1b54d542f86600c15315b97958c9002c0
thucDev <thuc.huynhtrung@bitis.com.vn>
2020-09-23 02:06:16 +0700

    done without test

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 import pyttsx3
-engine = pyttsx3.init()
-engine.say("I will speak this text")
-engine.runAndWait()
+import speech_recognition
+from datetime import date, datetime
+
+robot_mouth = pyttsx3.init()
+robot_ear = speech_recognition.Recognizer()
+robot_brain = ""
+
+while (True):
+    # hear
+    with speech_recognition.Microphone() as mic:
+        print("Robot: I'm listening")
+        audio = robot_ear.listen(mic)
+    try:
+        you = robot_ear.recognize_google(audio)
+    except:
+        you = ""
+    print("You" + you)
+
+    # brain
+    print("Robot: ... ")
+    if you == "":
+        robot_brain = "I cant hear you, try again"
+    elif "hello" in you:
+        robot_brain = "Hello Thuk"
+    elif "today" in you:
+        today = date.today()
+        robot_brain = today.strftime("%B %d, %Y")
+    elif "time" in you:
+        now = datetime.now()
+        robot_brain = now.strftime("%H hours %M minutes %S seconds")
+    elif "bye" in you:
+        robot_brain = "Goodbye, Thank"
+        robot_mouth.say("Robot: " + robot_brain)
+        robot_mouth.runAndWait()
+        break
+    # say
+    robot_mouth.say("Robot: " + robot_brain)
+    robot_mouth.runAndWait()
